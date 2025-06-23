d411535962afb53ec27c04ce22b5cdf2b81516b5
amandhakad13 <ad5538694@gmail.com>
2025-06-23 16:11:02 -0700

    create and print arraylist program 1

---
+++ collections_practice_programs/src/collections_practice_programs/ArrayListProgram1.java
@@ -0,0 +1,21 @@
+package collections_practice_programs;
+
+import java.util.ArrayList;
+
+public class ArrayListProgram1 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> al = new ArrayList<>();
+		
+		al.add("Red");
+		al.add("Blue");
+		al.add("Yellow");
+		al.add("Pink");
+		al.add("Purple");
+		
+		System.out.println(al);
+		
+	}
+
+}
